92f4864cef1aabe99da9362ffe5d30f9b0fc8d3c
Dmitry Ryzhkov <dimaryz@gmail.com>
2023-12-18 14:21:03 -0800

    day 15 part 2

--- src/15/index.ts
+++ src/15/index.ts
@@ -1,6 +1,26 @@
 import fs from 'fs';
 
-function parseData(): string[] {
+type Sequence = DeleteSequence | SetSequence;
+
+type DeleteSequence = {
+    operation: '-';
+    label: string;
+};
+
+type SetSequence = {
+    operation: '=';
+    label: string;
+    focalLength: number;
+};
+
+type Lens = {
+    label: string;
+    focalLength: number;
+};
+
+type Box = Lens[][];
+
+function parseData(): Sequence[] {
     let data = '';
     try {
         data = fs.readFileSync('src/15/real.txt', 'utf8');
@@ -8,7 +28,24 @@ function parseData(): string[] {
         console.error('Error reading the file:', err);
     }
 
-    return data.split(',');
+    return data.split(',').map((value) => {
+        if (value.indexOf('-') > -1) {
+            const pieces = value.split('-');
+            return {
+                label: pieces[0],
+                operation: '-'
+            };
+        } else if (value.indexOf('=') > -1) {
+            const pieces = value.split('=');
+            return {
+                label: pieces[0],
+                operation: '=',
+                focalLength: Number(pieces[1])
+            };
+        } else {
+            throw new Error('invalid sequence: ' + value);
+        }
+    });
 }
 
 function hash(input: string): number {
@@ -25,13 +62,43 @@ function hash(input: string): number {
 }
 
 export function solvePuzzle() {
-    const sequence = parseData();
+    const sequences = parseData();
+    const boxes: Box = new Array(256);
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i] = [];
+    }
 
-    let totalSum = 0;
+    sequences.forEach((sequence) => {
+        const index = hash(sequence.label);
+        const box = boxes[index];
+        const targetIndex = box.findIndex((lens) => lens.label === sequence.label);
 
-    sequence.forEach((value) => {
-        totalSum += hash(value);
+        if (targetIndex > -1) {
+            if (sequence.operation === '=') {
+                box[targetIndex] = {
+                    label: sequence.label,
+                    focalLength: sequence.focalLength
+                };
+            } else {
+                box.splice(targetIndex, 1);
+            }
+        } else {
+            if (sequence.operation === '=') {
+                box.push({
+                    label: sequence.label,
+                    focalLength: sequence.focalLength
+                });
+            }
+        }
     });
 
-    return totalSum;
+    let focusingPower = 0;
+
+    boxes.forEach((box, boxIndex) => {
+        box.forEach((lens, lensIndex) => {
+            focusingPower += (1 + boxIndex) * (1 + lensIndex) * lens.focalLength;
+        });
+    });
+
+    return focusingPower;
 }
